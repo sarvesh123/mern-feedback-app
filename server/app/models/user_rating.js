20ab4f73b76d07554b64f67a995826b39cac5e80
Sarvesh Shejwadkar <sarveshshejwadkar@gmail.com>
2018-12-02 22:06:01 +0530

    insert of update user rating

--- server/app/models/user_rating.js
+++ server/app/models/user_rating.js
@@ -2,12 +2,17 @@ const UserRatingsDao = require('../dao/user_ratings')
 const mongoose = require('mongoose')
 
 exports.update_user_ratings = function (req, res) {
-    let conditions = {user_id: mongoose.Types.ObjectId(req.user_id)}
+    let conditions = {
+        user_id: mongoose.Types.ObjectId(req.user_id),
+        attribute: mongoose.Types.ObjectId(req.attribute_id)
+    }
     let update = { 
-        attribute: new mongoose.Types.ObjectId(req.attribute),
         rating: req.rating
     }
-    let options = { 'new': true }
+    let options = {
+        'new': true,
+        'upsert': true
+    }
     UserRatingsDao.findOneAndUpdate(conditions, update, options, function (err, user_ratings) {
         if (err) {
             res.send(err)
